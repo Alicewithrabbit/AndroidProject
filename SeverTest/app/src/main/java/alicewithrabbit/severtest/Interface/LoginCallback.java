2241b8fa506e39783d8ac39d3f42aaf80cd2c190
Alicewithrabbit <althaswc@qq.com>
2016-10-05 23:11:18 +0800

    1

--- SeverTest/app/src/main/java/alicewithrabbit/severtest/Interface/LoginCallback.java
+++
@@ -1,9 +0,0 @@
-package alicewithrabbit.severtest.Interface;
-
-/**
- * Created by Alicewithrabbit on 2016/10/3 0003.
- */
-
-public interface LoginCallback {
-    public void onLogin(boolean success);
-}
--- SeverTest/app/src/main/java/alicewithrabbit/severtest/Interface/OnReceiverListener.java
+++
@@ -1,8 +0,0 @@
-package alicewithrabbit.severtest.Interface;
-
-/**
- * Created by Alicewithrabbit on 2016/10/3 0003.
- */
-public interface OnReceiverListener {
-    public void onReceiver(String user,String msg);
-}
